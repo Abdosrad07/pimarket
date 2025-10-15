a92ae0fb7ee70cf847f5da5680eb214f8fd145e8
Abdosrad07 <mnjimongbafochive201@gmail.com>
2025-10-15 16:30:17 +0100

    commit

--- pimarket/settings.py
+++ pimarket/settings.py
@@ -115,8 +115,13 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = 'static/'
+
+# Assurez-vous que le dossier static existe avant de l'ajouter
+STATICFILES_DIRS = []
+if (BASE_DIR / 'static').exists():
+    STATICFILES_DIRS.append(BASE_DIR / 'static')
+
 STATIC_ROOT = BASE_DIR / 'staticfiles' 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
