7dd46fdf0461aee8713e833118c700ee38b1411b
Abdosrad07 <mnjimongbafochive201@gmail.com>
2025-10-15 21:35:07 +0100

    essai

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import serve_text_file
 
 urlpatterns = [
-    path('<str:filename>', serve_text_file, name='serve_text_file'),
+    path('validation-key.txt', serve_text_file, name='validation_key'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse, Http404
 from django.conf import settings
 import os
 
-def serve_text_file(request, filename):
-    file_path = os.path.join(settings.BASE_DIR, 'static', filename)
+def serve_text_file(request):
+    file_path = os.path.join(settings.BASE_DIR, 'static', 'validation-key.txt')
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
