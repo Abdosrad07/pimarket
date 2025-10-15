2c82859dfe5ba0ffdf9f33038273f07fcad61b44
Abdosrad07 <mnjimongbafochive201@gmail.com>
2025-10-15 21:47:02 +0100

    encore

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import serve_text_file
 
 urlpatterns = [
-    path('validation-key.txt', serve_text_file, name='validation_key'),
+    path('validation-key', serve_text_file, name='validation_key'),
 ]
