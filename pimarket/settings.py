68c3081037b7eb254561fd63c013de98b9fddcbe
Abdosrad07 <mnjimongbafochive201@gmail.com>
2025-10-15 22:02:36 +0100

    encore

--- pimarket/settings.py
+++ pimarket/settings.py
@@ -119,7 +119,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Assurez-vous que le dossier static existe avant de l'ajouter
 STATICFILES_DIRS = [
